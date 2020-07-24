fd6e7ceb478fa3b062e41600e76b3c1a8bd30d6d
Jhonny Chamoun <jhonny.chamoun@gmail.com>
2020-07-24 18:47:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,10 +4,11 @@ def load_library
   # code goes here
   library = {}
   emoticons = YAML.load_file('emoticons.yml')
-  emoticons.each{ |emoticon|
-    library[emoticon][:english] = emoticon[0]
-    library[emoticon][:japanese] = emoticon[1]
+  emoticons.each{ |emoticon, translation|
+    library[emoticon][:english] = translation[0]
+    library[emoticon][:japanese] = translation[1]
   }
+  library
 end
 
 def get_japanese_emoticon
